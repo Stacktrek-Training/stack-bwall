4899e7b4ebb09ca7a6d8bd80e2a1fe13378c4eb8
edvnsn2609 <Edvensonmgiuel.com>
2023-04-20 13:57:32 +0800

    Commit message describing the changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
   content: [
     "./src/**/*.{html,js,jsx,ts,tsx}",
   "./node_modules/flowbite/**/*.js",
-  "./node_modules/tw-elements/dist/js/**/*.js",
-  'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  "./node_modules/tw-elements/dist/js/**/*.js"
 ],
 
   theme: {
